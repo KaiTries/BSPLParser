52744832ece72d4bb1b9d8aee14d1a30b6882b75
Kai <112092835+KaiTries@users.noreply.github.com>
2025-05-07 14:04:01 +0200

    tests: added test for 2 protocols in one file

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import Parser.BSPLClasses.BSPLProtocol;
 import Parser.BSPLParser;
 import Tokenizer.BSPLTokenizer;
 import java.io.IOException;
@@ -9,12 +10,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TestParser {
 
   @Test
-  public void testReadFile() throws IOException, URISyntaxException {
+  public void testReadFileLogistics() throws IOException, URISyntaxException {
     // Test the parseFile method
     URL resource = getClass().getClassLoader().getResource("example_logistics.bspl");
     assertNotNull(resource);
@@ -24,6 +26,35 @@ public class TestParser {
 
     final BSPLTokenizer tokenizer = new BSPLTokenizer(fileString);
     final BSPLParser parser = new BSPLParser(tokenizer.tokenize());
-    parser.parse();
+    final List<BSPLProtocol> protocols = parser.parse();
+    System.out.println(protocols);
+  }
+  @Test
+  public void testReadFileExample() throws IOException, URISyntaxException {
+    // Test the parseFile method
+    URL resource = getClass().getClassLoader().getResource("example.bspl");
+    assertNotNull(resource);
+    Path path = Paths.get(resource.toURI());
+    final String fileString =
+        Files.readString(path);
+
+    final BSPLTokenizer tokenizer = new BSPLTokenizer(fileString);
+    final BSPLParser parser = new BSPLParser(tokenizer.tokenize());
+    final List<BSPLProtocol> protocols = parser.parse();
+    System.out.println(protocols);
+  }
+  @Test
+  public void testReadFileTwoProtocols() throws IOException, URISyntaxException {
+    // Test the parseFile method
+    URL resource = getClass().getClassLoader().getResource("example_two_protocols.bspl");
+    assertNotNull(resource);
+    Path path = Paths.get(resource.toURI());
+    final String fileString =
+        Files.readString(path);
+
+    final BSPLTokenizer tokenizer = new BSPLTokenizer(fileString);
+    final BSPLParser parser = new BSPLParser(tokenizer.tokenize());
+    final List<BSPLProtocol> protocols = parser.parse();
+    System.out.println(protocols);
   }
 }
