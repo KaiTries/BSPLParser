e65f46d342753727285b02f7646c8689573cb679
Kai Schultz <kai.schultz@bluewin.ch>
2025-05-21 12:24:57 +0200

    chore: javadocs

--- src/main/java/BSPLValidatorListener.java
+++ src/main/java/BSPLValidatorListener.java
@@ -4,7 +4,14 @@ import java.util.Set;
 
 public class BSPLValidatorListener extends BSPLBaseListener {
     private final Set<String> definedRoles = new HashSet<>();
+    private final Set<String> definedParams = new HashSet<>();
 
+
+    /**
+     * Checks that atleast one of the parameters is a key.
+     *
+     * @param ctx the parse tree
+     */
     @Override
     public void exitParams(BSPLParser.ParamsContext ctx) {
         boolean hasKey = false;
@@ -12,12 +19,18 @@ public class BSPLValidatorListener extends BSPLBaseListener {
             if (param.getText().contains("key")) {
                 hasKey = true;
             }
+            definedParams.add(param.name.getText());
         }
         if (!hasKey) {
             System.err.println("Error: 'key' parameter is missing in the parameters list.");
         }
     }
 
+    /**
+     * Checks that the roles are defined and not duplicated.
+     *
+     * @param ctx the parse tree
+     */
     @Override
     public void enterRoles(BSPLParser.RolesContext ctx) {
         for (BSPLParser.RoleContext role : ctx.role()) {
@@ -30,6 +43,13 @@ public class BSPLValidatorListener extends BSPLBaseListener {
         }
     }
 
+    /**
+     * Checks that the message sender and recipient are defined roles,
+     * and that parameters exist.
+     *
+     *
+     * @param ctx the parse tree
+     */
     @Override
     public void enterMessage(BSPLParser.MessageContext ctx) {
         String sender = ctx.sender.getText();
@@ -41,5 +61,13 @@ public class BSPLValidatorListener extends BSPLBaseListener {
         if (!definedRoles.contains(recipient)) {
             System.err.println("Error: Recipient role '" + recipient + "' is not defined.");
         }
+
+        for (BSPLParser.ParamContext param : ctx.params().param()) {
+            if (!definedParams.contains(param.name.getText())) {
+                System.err.println("Error: Parameter '" + param.getText() + "' is not defined.");
+            }
+        }
+
+
     }
 }
